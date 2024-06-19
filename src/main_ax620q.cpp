901b6b1fe1a03efe8886f6748a2fa6a8f9d8ce0b
ZHEQIUSHUI <qiushui.zhe@gmail.com>
2024-06-19 09:56:16 +0800

    add ax620q main code

--- src/main_ax620q.cpp
+++ src/main_ax620q.cpp
@@ -1,8 +1,134 @@
-#include "Runner/CLIPAX650.hpp"
-#include "Runner/CLIPOnnx.hpp"
+// #include "Runner/CLIPAX650.hpp"
+// #include "Runner/CLIPOnnx.hpp"
+#include <memory>
+#include <fstream>
 
+#include "opencv2/opencv.hpp"
 #include "string_utility.hpp"
 #include "cmdline.hpp"
+#include "sample_log.h"
+#include "ax_model_runner_ax650.hpp"
+
+class ClipDecoder
+{
+public:
+    static void softmax(const std::vector<std::vector<float>> &input, std::vector<std::vector<float>> &result)
+    {
+        result.reserve(input.size());
+
+        for (const auto &row : input)
+        {
+            std::vector<float> rowResult;
+            rowResult.reserve(row.size());
+
+            float maxVal = *std::max_element(row.begin(), row.end());
+
+            float sumExp = 0.0;
+            for (float val : row)
+            {
+                float expVal = std::exp(val - maxVal);
+                rowResult.emplace_back(expVal);
+                sumExp += expVal;
+            }
+
+            for (float &val : rowResult)
+            {
+                val /= sumExp;
+            }
+
+            result.emplace_back(std::move(rowResult));
+        }
+    }
+
+    static void forward(
+        const std::vector<std::vector<float>> &imageFeatures, const std::vector<std::vector<float>> &textFeatures,
+        std::vector<std::vector<float>> &logits_per_image, std::vector<std::vector<float>> &logits_per_text)
+    {
+        std::vector<std::vector<float>> logitsPerImage;
+        logitsPerImage.reserve(imageFeatures.size());
+
+        for (const auto &_row : imageFeatures)
+        {
+            float norm = 0.0;
+            for (float val : _row)
+            {
+                norm += val * val;
+            }
+            norm = std::sqrt(norm);
+            std::vector<float> normRow;
+            normRow.reserve(_row.size());
+            for (float val : _row)
+            {
+                normRow.push_back(val / norm);
+            }
+
+            std::vector<float> row;
+            row.reserve(textFeatures.size());
+            for (const auto &textRow : textFeatures)
+            {
+                float sum = 0.0;
+                for (size_t i = 0; i < normRow.size(); i++)
+                {
+                    sum += normRow[i] * textRow[i];
+                }
+                row.push_back(100 * sum);
+            }
+            logitsPerImage.push_back(std::move(row));
+        }
+
+        std::vector<std::vector<float>> logitsPerText(logitsPerImage[0].size(), std::vector<float>(logitsPerImage.size()));
+
+        for (size_t i = 0; i < logitsPerImage.size(); i++)
+        {
+            for (size_t j = 0; j < logitsPerImage[i].size(); j++)
+            {
+                logitsPerText[j][i] = logitsPerImage[i][j];
+            }
+        }
+
+        softmax(logitsPerImage, logits_per_image);
+        softmax(logitsPerText, logits_per_text);
+    }
+};
+
+class CLIPAX650
+{
+private:
+    std::shared_ptr<ax_runner_base> m_encoder;
+    cv::Mat input;
+    int input_height;
+    int input_width;
+    int LEN_IMAGE_FEATURE;
+
+public:
+    bool load_image_encoder(std::string encoder_path)
+    {
+        m_encoder.reset(new ax_runner_ax650);
+        m_encoder->init(encoder_path.c_str());
+        input_height = m_encoder->get_algo_height();
+        input_width = m_encoder->get_algo_width();
+        ALOGI("input size %d %d", input_height, input_width);
+        input = cv::Mat(input_height, input_width, CV_8UC3, m_encoder->get_input(0).pVirAddr);
+
+        LEN_IMAGE_FEATURE = m_encoder->get_output(0).vShape[1];
+        ALOGI("image feature len %d", LEN_IMAGE_FEATURE);
+        return true;
+    }
+    void encode(cv::Mat image, std::vector<float> &image_features)
+    {
+        if (!m_encoder.get())
+        {
+            ALOGE("encoder not init");
+            return;
+        }
+        cv::resize(image, input, cv::Size(input_width, input_height));
+        cv::cvtColor(input, input, cv::COLOR_BGR2RGB);
+        auto ret = m_encoder->inference();
+
+        image_features.resize(LEN_IMAGE_FEATURE);
+        memcpy(image_features.data(), m_encoder->get_output(0).pVirAddr, LEN_IMAGE_FEATURE * sizeof(float));
+    }
+};
 
 bool _file_exist(const std::string &path)
 {
@@ -54,25 +180,13 @@ bool _file_dump(const std::string &path, char *data, int size)
     return true;
 }
 
-void process_texts(std::shared_ptr<CLIP> &mClip, std::vector<std::string> &lines, std::vector<std::string> &texts, std::vector<std::vector<float>> &text_features)
+void process_texts(std::vector<std::string> &lines, std::vector<std::string> &texts, std::vector<std::vector<float>> &text_features)
 {
     for (size_t i = 0; i < lines.size(); i++)
     {
         auto &s = lines[i];
         std::vector<std::string> ctxs = string_utility<std::string>::split(s, ":");
-        if (ctxs.size() == 1)
-        {
-            std::vector<std::vector<float>> tmp_text_features;
-
-            auto time_start = std::chrono::high_resolution_clock::now();
-            mClip->encode(ctxs, tmp_text_features);
-            auto time_end = std::chrono::high_resolution_clock::now();
-            ALOGI("encode text [%s] cost time : %f", ctxs[0].c_str(), std::chrono::duration<double>(time_end - time_start).count());
-
-            texts.push_back(ctxs[0]);
-            text_features.insert(text_features.end(), tmp_text_features.begin(), tmp_text_features.end());
-        }
-        else if (ctxs.size() == 2)
+        if (ctxs.size() == 2)
         {
             if (_file_exist(ctxs[1]))
             {
@@ -89,18 +203,7 @@ void process_texts(std::shared_ptr<CLIP> &mClip, std::vector<std::string> &lines
             }
             else
             {
-                std::vector<std::string> tmp_texts{ctxs[0]};
-                std::vector<std::vector<float>> tmp_text_features;
-                auto time_start = std::chrono::high_resolution_clock::now();
-                mClip->encode(tmp_texts, tmp_text_features);
-                auto time_end = std::chrono::high_resolution_clock::now();
-                ALOGI("encode text [%s] cost time : %f", ctxs[0].c_str(), std::chrono::duration<double>(time_end - time_start).count());
-
-                texts.push_back(ctxs[0]);
-                text_features.insert(text_features.end(), tmp_text_features.begin(), tmp_text_features.end());
-
-                _file_dump(ctxs[1], (char *)tmp_text_features[0].data(), tmp_text_features[0].size() * sizeof(float));
-                ALOGI("write text feature [%s] to %s", ctxs[0].c_str(), ctxs[1].c_str());
+                ALOGE("text file not exist, %s", ctxs[1].c_str());
             }
         }
         else
@@ -115,32 +218,19 @@ int main(int argc, char *argv[])
 {
     std::string image_src;
     std::string text_src;
-    std::string vocab_path;
     std::string image_encoder_model_path;
-    std::string text_encoder_model_path;
-    int language = 0;
 
     cmdline::parser cmd;
     cmd.add<std::string>("ienc", 0, "encoder model(onnx model or axmodel)", true, image_encoder_model_path);
-    cmd.add<std::string>("tenc", 0, "text encoder model(onnx model or axmodel)", false, text_encoder_model_path);
     cmd.add<std::string>("image", 'i', "image file or folder(jpg png etc....)", true, image_src);
-    cmd.add<std::string>("text", 't', "text or txt file", true, text_src);
-    cmd.add<std::string>("vocab", 'v', "vocab path", true, vocab_path);
-    cmd.add<int>("language", 'l', "language choose, 0:english 1:chinese", true, 0);
+    cmd.add<std::string>("text", 't', "txt file with content like \n\n%s    dog:text_feat/dog.bin\n    bird:text_feat/bird.bin\n    cat:text_feat/cat.bin\n", true, text_src);
 
     cmd.parse_check(argc, argv);
 
-    vocab_path = cmd.get<std::string>("vocab");
     image_encoder_model_path = cmd.get<std::string>("ienc");
-    text_encoder_model_path = cmd.get<std::string>("tenc");
-    language = cmd.get<int>("language");
 
-    std::shared_ptr<CLIP> mClip;
-    if (string_utility<std::string>::ends_with(image_encoder_model_path, ".onnx"))
-    {
-        mClip.reset(new CLIPOnnx);
-    }
-    else if (string_utility<std::string>::ends_with(image_encoder_model_path, ".axmodel"))
+    std::shared_ptr<CLIPAX650> mClip;
+    if (string_utility<std::string>::ends_with(image_encoder_model_path, ".axmodel"))
     {
         mClip.reset(new CLIPAX650);
     }
@@ -151,20 +241,9 @@ int main(int argc, char *argv[])
     }
 
     mClip->load_image_encoder(image_encoder_model_path);
-    if (!text_encoder_model_path.empty())
-    {
-        mClip->load_text_encoder(text_encoder_model_path);
-    }
-    else
     {
         ALOGI("if you dont want to load text encoder, the '--text' args must be set like '--text dog:dog.bin', or set a txt file with content like \n\n%s    dog:dog.bin\n    bird:bird.bin\n    cat:cat.bin\n", MACRO_RED);
         ALOGI("and make sure the '.bin' file exist\n");
-    }
-
-    if(!mClip->load_tokenizer(vocab_path, language == 1))
-    {
-        ALOGE("load tonkenizer failed");
-        return -1;
     }
 
     image_src = cmd.get<std::string>("image");
@@ -194,7 +273,7 @@ int main(int argc, char *argv[])
         lines.push_back(text_src);
     }
 
-    process_texts(mClip, lines, texts, text_features);
+    process_texts(lines, texts, text_features);
 
     // auto time_start = std::chrono::high_resolution_clock::now();
     // mClip->encode(texts, text_features);
@@ -241,7 +320,7 @@ int main(int argc, char *argv[])
 
     std::vector<std::vector<float>> logits_per_image, logits_per_text;
     auto time_start = std::chrono::high_resolution_clock::now();
-    mClip->decode(image_features, text_features, logits_per_image, logits_per_text);
+    ClipDecoder::forward(image_features, text_features, logits_per_image, logits_per_text);
     auto time_end = std::chrono::high_resolution_clock::now();
     auto diff = time_end - time_start;
     std::cout << "postprocess cost time : " << std::chrono::duration<double>(diff).count() << "s" << std::endl;
